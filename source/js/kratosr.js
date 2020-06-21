7d50e466679404bac57bdc2a2bebc57fa545c051
Candinya <dev@lcy.moe>
2020-06-22 01:58:12 +0800

    版本大更新（？）
    1. 滚动条样式修复（Disqus因为用的是iframe，暂时还没有注入修复）
    2. 侧边栏定位修复（我真傻，真的，有那么好的sticky放着不用，去手搓JS自找虐）
    3. 暂时禁用了打赏与文章隐藏块展开功能（bug很多，还在修整）
    4. 分享功能的图片换成了CSS样式
    5. 加入暗色模式适配（测试中）
    6. 细节调整与修复

--- source/js/kratosr.js
+++ source/js/kratosr.js
@@ -8,7 +8,6 @@ var kr = new Object();
     kr.wechat = "/images/wechatpayqr.png";
     kr.copy_notify = false;
     kr.copy_notify_text = "欢迎转载，请记得标明出处哦~";
-    kr.site_logo = "images/favicon.png";
     kr.enable_site_leave_event = false;
     kr.site_logo_leave = "images/failure.ico";
     kr.site_title_leave = "{{{(>_<)}}}哦哟，崩溃啦~ ";
@@ -18,40 +17,6 @@ var kr = new Object();
 (function(){
     let shareMenu = ()=>{
         $(document).on("click",".Share",()=>{$(".share-wrap").fadeToggle("slow");});
-    };
-
-    var mainOffset;
-    let sidebarAffixInit = ()=>{
-        $('#sidebar').removeClass('affix');
-        $('#sidebar').removeClass('affix-top');
-        $('#sidebar').removeClass('affix-bottom');
-        $('#sidebar').css('top', '');
-        mainOffset = $('#main').offset().top;
-    };
-    let sidebarAffix = ()=>{
-        sidebarAffixInit();
-        if ($("#sidebar").height()){
-            $(window).scroll(()=>{
-                if ($('#main').children('section').outerHeight(true) > $('#sidebar').outerHeight(true)) {
-                    if ($(window).scrollTop() < mainOffset - 56){
-                        $('#sidebar').addClass('affix-top');
-                        $('#sidebar').removeClass('affix');
-                        $('#sidebar').css('top', '');
-                    } else if ($(window).scrollTop() > $('#footer').offset().top - $('#sidebar').outerHeight(true) - 56){
-                        $('#sidebar').addClass('affix-bottom');
-                        $('#sidebar').removeClass('affix');
-                        $('#sidebar').css('top', $('#footer').offset().top - $('#sidebar').outerHeight(true) - 345 + 'px');
-                    } else {
-                        $('#sidebar').addClass('affix');
-                        $('#sidebar').removeClass('affix-top');
-                        $('#sidebar').removeClass('affix-bottom');
-                        $('#sidebar').css('top', '');
-                    }
-                } else {
-                    sidebarAffixInit();
-                }
-            });
-        }
     };
     
     // var toSearch = function(){
@@ -66,7 +31,7 @@ var kr = new Object();
     //     });
     //     $('#searchform').on("click",function(e){e.stopPropagation();})
     // }
-    let gotop = ()=>{
+    let gotopInit = ()=>{
         $('.gotop-box').on('click',function(event){
             event.preventDefault();
             $('html, body').animate({
@@ -76,9 +41,9 @@ var kr = new Object();
         });
         $(window).scroll(()=>{
             if ($(window).scrollTop()>200){
-                $('.cd-tool').addClass('scrollDown');
+                $('.kr-tool').addClass('scroll-down');
             }else{
-                $('.cd-tool').removeClass('scrollDown');
+                $('.kr-tool').removeClass('scroll-down');
             }
         });
     };
@@ -125,35 +90,35 @@ var kr = new Object();
             }
         });
     };
-    let xControl = ()=>{
-        $(document).on("click",".xHeading",()=>{
-            var $this = $(this);
-            $this.closest('.xControl').filter('.xContent').slideToggle(300);
-            if ($this.closest('.xControl').hasClass('active')){
-                $this.closest('.xControl').removeClass('active');
-            }else{
-                $this.closest('.xControl').addClass('active');
-            }
-            event.preventDefault();
-        });
-    };
-    let donateConfig = ()=>{
-        $(document).on("click",".Donate",()=>{
-            layer.open({
-                type:1,
-                area:['300px', '370px'],
-                title:kr.donate,
-                resize:false,
-                scrollbar:false,
-                content:'<div class="donate-box"><div class="meta-pay text-center"><strong>'+kr.scan+'</strong></div><div class="qr-pay text-center"><img class="pay-img" id="alipay_qr" src="'+kr.alipay+'"><img class="pay-img d-none" id="wechat_qr" src="'+kr.wechat+'"></div><div class="choose-pay text-center mt-2"><input id="alipay" type="radio" name="pay-method" checked><label for="alipay" class="pay-button"><img src="' + kr.thome + 'images/alipay.png"></label><input id="wechatpay" type="radio" name="pay-method"><label for="wechatpay" class="pay-button"><img src="' + kr.thome + 'images/wechat.png"></label></div></div>'
-            });
-            $(".choose-pay input[type='radio']").click(()=>{
-                var id= $(this).attr("id");
-                if(id=='alipay'){$(".qr-pay #alipay_qr").removeClass('d-none');$(".qr-pay #wechat_qr").addClass('d-none')};
-                if(id=='wechatpay'){$(".qr-pay #alipay_qr").addClass('d-none');$(".qr-pay #wechat_qr").removeClass('d-none')};
-            });
-        });
-    };
+    // let xControl = ()=>{
+    //     $(document).on("click",".xHeading",()=>{
+    //         var $this = $(this);
+    //         $this.closest('.xControl').filter('.xContent').slideToggle(300);
+    //         if ($this.closest('.xControl').hasClass('active')){
+    //             $this.closest('.xControl').removeClass('active');
+    //         }else{
+    //             $this.closest('.xControl').addClass('active');
+    //         }
+    //         event.preventDefault();
+    //     });
+    // };
+    // let donateConfig = ()=>{
+    //     $(document).on("click",".Donate",()=>{
+    //         layer.open({
+    //             type:1,
+    //             area:['300px', '370px'],
+    //             title:kr.donate,
+    //             resize:false,
+    //             scrollbar:false,
+    //             content:'<div class="donate-box"><div class="meta-pay text-center"><strong>'+kr.scan+'</strong></div><div class="qr-pay text-center"><img class="pay-img" id="alipay_qr" src="'+kr.alipay+'"><img class="pay-img d-none" id="wechat_qr" src="'+kr.wechat+'"></div><div class="choose-pay text-center mt-2"><input id="alipay" type="radio" name="pay-method" checked><label for="alipay" class="pay-button"><img src="' + kr.thome + 'images/alipay.png"></label><input id="wechatpay" type="radio" name="pay-method"><label for="wechatpay" class="pay-button"><img src="' + kr.thome + 'images/wechat.png"></label></div></div>'
+    //         });
+    //         $(".choose-pay input[type='radio']").click(()=>{
+    //             var id = $(this).attr("id");
+    //             if(id=='alipay'){$(".qr-pay #alipay_qr").removeClass('d-none');$(".qr-pay #wechat_qr").addClass('d-none')};
+    //             if(id=='wechatpay'){$(".qr-pay #alipay_qr").addClass('d-none');$(".qr-pay #wechat_qr").removeClass('d-none')};
+    //         });
+    //     });
+    // };
 
     let setrandpic = ()=>{
         //图片
@@ -195,40 +160,38 @@ var kr = new Object();
 
     $.fn.pjax_reload = ()=>{
         setrandpic();
-        sidebarAffixInit();
         fancyboxInit();
     };
 
-    
-
     $(()=>{
         shareMenu();
-        gotop();
+        gotopInit();
         // toSearch();
         offcanvas();
         mobiClick();
-        xControl();
-        donateConfig();
-        sidebarAffix();
+        // xControl();
+        // donateConfig();
         tocNavInit();
         $(this).pjax_reload();
     });
 }());
 
-var now = new Date();
-function createtime(){
-    var grt = new Date(kr.ctime);
-    now.setTime(now.getTime()+1000);
-    days = (now-grt)/1000/60/60/24;dnum = Math.floor(days);
-    hours = (now-grt)/1000/60/60-(24*dnum);hnum = Math.floor(hours);
-    if(String(hnum).length==1){hnum = "0"+hnum;}
-    minutes = (now-grt)/1000/60-(24*60*dnum)-(60*hnum);mnum = Math.floor(minutes);
-    if(String(mnum).length==1){mnum = "0"+mnum;}
-    seconds = (now-grt)/1000-(24*60*60*dnum)-(60*60*hnum)-(60*mnum);snum = Math.round(seconds);
-    if(String(snum).length==1){snum = "0"+snum;}
-    document.getElementById("span_dt_dt").innerHTML = dnum+"天"+hnum+"小时"+mnum+"分"+snum+"秒";
+{
+    var now = new Date();
+    let grt = new Date(kr.ctime);
+    let upTimeNode = document.getElementById("span_dt");
+    setInterval(()=>{
+        now.setTime(now.getTime()+1000);
+        days = (now-grt)/1000/60/60/24;dnum = Math.floor(days);
+        hours = (now-grt)/1000/60/60-(24*dnum);hnum = Math.floor(hours);
+        if(String(hnum).length==1){hnum = "0"+hnum;}
+        minutes = (now-grt)/1000/60-(24*60*dnum)-(60*hnum);mnum = Math.floor(minutes);
+        if(String(mnum).length==1){mnum = "0"+mnum;}
+        seconds = (now-grt)/1000-(24*60*60*dnum)-(60*60*hnum)-(60*mnum);snum = Math.round(seconds);
+        if(String(snum).length==1){snum = "0"+snum;}
+        upTimeNode.innerText = dnum+"天"+hnum+"小时"+mnum+"分"+snum+"秒";
+    }, 1000);
 }
-setInterval("createtime()",1000);
 
 if (kr.copy_notify) {
     document.body.oncopy = ()=>{alert(kr.copy_notify_text);}
@@ -236,21 +199,20 @@ if (kr.copy_notify) {
 
 window.onload = ()=>{
     console.log('%c页面加载完毕消耗了'+Math.round(performance.now()*100)/100+'ms','background:#fff;color:#333;text-shadow:0 0 2px #eee,0 0 3px #eee,0 0 3px #eee,0 0 2px #eee,0 0 3px #eee;');
-    // if (!(notMobile)) {
-    //     $('.group-box').attr("style", "display:none");
-    // }
 };
 
 if (kr.enable_site_leave_event) {
-    var OriginTitile = document.title, titleTime;
+    var OriginTitile, titleTime;
+    let OriginLogo = $('[rel="icon"]').attr("href");
     document.addEventListener('visibilitychange', ()=>{
         if (document.hidden) {
+            OriginTitile = document.title;
             document.title = kr.site_title_leave + OriginTitile;
             $('[rel="icon"]').attr("href", kr.thome + kr.site_logo_leave);
             clearTimeout(titleTime);
         } else {
             document.title = kr.site_title_return + OriginTitile;
-            $('[rel="icon"]').attr("href", kr.thome + kr.site_logo);
+            $('[rel="icon"]').attr("href", OriginLogo);
             titleTime = setTimeout(()=>{
                 document.title = OriginTitile;
             }, 2000);
