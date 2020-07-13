261cd00d4ee3ae1f75d78beef7fe54bfab4d2578
Candinya <dev@lcy.moe>
2020-07-13 13:33:21 +0800

    调整了扩展标签的写法

--- scripts/tag-widgets.js
+++ scripts/tag-widgets.js
@@ -6,35 +6,30 @@
 
 // 提示横幅
 hexo.extend.tag.register('alertbox', function(args){
-    return '<div class="alert alert-' + args[0] + '">' + args[1] + '</div>';
+    return `<div class="alert alert-${args[0]}">${args[1]}</div>`;
 });
 
 // 折叠内容
-hexo.extend.tag.register('xchead', function(args){
-    return `<div class="xControl">
-    <div class="xHeading">
-    <div class="xIcon"><i class="fa fa-plus"></i></div>
-    <span>` + args[0] + `</span>
-    </div>
-    <div class="xContent">
-    <div class="inner">`;
-});
+hexo.extend.tag.register('collapse', function(args, content){
+    content = hexo.render.renderSync({text: content, engine: 'markdown'});
 
-hexo.extend.tag.register('xcfoot', function(){
-    return `</div>
-    </div>
+    return `<div class="xControl">
+    <div class="xHeading"><div class="xIcon"><i class="fa fa-plus"></i></div><span>${args[0]}</span></div>
+    <div class="xContent"><div class="inner">
+        ${content} 
+    </div></div>
     </div>`;
-});
+}, {ends: true});
 
 // 提示面板
-hexo.extend.tag.register('panelhead', function(args){
-    return `<div class="panel panel-` + args[0] + `">
-    <div class="panel-title">` + args[1] + `</div>
-    <div class="panel-body">`;
-});
+hexo.extend.tag.register('colorpanel', function(args, content){
+    content = hexo.render.renderSync({text: content, engine: 'markdown'});
 
-hexo.extend.tag.register('panelfoot', function(){
-    return `</div>
+    return `<div class="panel panel-${args[0]}">
+    <div class="panel-title">${args[1]}</div>
+    <div class="panel-body">
+        ${content}
+    </div>
     </div>`;
-});
+}, {ends: true});
 
