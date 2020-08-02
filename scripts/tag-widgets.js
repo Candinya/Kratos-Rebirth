0a83682ed8cce462604a36c56395ebed8548e585
Candinya <git@candinya.com>
2020-08-03 01:15:13 +0800

    部分累积更新
    1. 删除了无标题文章在侧边栏小工具的显示
    2. 归档页面微调
    3. 搜索页面加入搜索提示
    4. 部分样式细节调整与修复
    5. 调整og的生成方式（使用Hexo的辅助函数）

--- scripts/tag-widgets.js
+++ scripts/tag-widgets.js
@@ -6,8 +6,8 @@
 
 // 提示横幅
 hexo.extend.tag.register('alertbox', function(args){
-    const content = hexo.render.renderSync({text: args[1], engine: 'markdown'});
-    return `<div class="alert alert-${args[0]}">${content}</div>`;
+    // const content = hexo.render.renderSync({text: args[1], engine: 'markdown'});
+    return `<div class="alert alert-${args[0]}">${args[1]}</div>`;
 });
 
 // 折叠内容
