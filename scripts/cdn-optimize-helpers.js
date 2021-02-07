5aa3d1bffdcc441eb99b1a13c890ea9d41515b78
Candinya <dev@candinya.com>
2021-02-07 11:58:40 +0800

    update: 优化辅助函数写法（单出口）

--- scripts/cdn-optimize-helpers.js
+++ scripts/cdn-optimize-helpers.js
@@ -1,10 +1,10 @@
 // 使用CDN提供静态资源，改写URL
 hexo.theme.on('processAfter', () => {
     hexo.extend.helper.register('url_cdn', (file) => {
-        if (hexo.theme.config.cdn) {
-            return 'https://cdn.jsdelivr.net/gh/Candinya/Kratos-Rebirth@latest/source/' + file;
-        } else {
-            return hexo.config.root + file;
-        }
+        return (
+            hexo.theme.config.cdn
+            ? 'https://cdn.jsdelivr.net/gh/Candinya/Kratos-Rebirth@latest/source/'
+            : hexo.config.root
+        ) + file;
     });
 });
