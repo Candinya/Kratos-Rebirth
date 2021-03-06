e8d7b3eb93641f83678925475e8e4e824b5acd14
Candinya <dev@candinya.com>
2021-03-06 14:33:33 +0800

    BREAKING CHANGE: 使用SASS重写样式文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const { src, dest, parallel, watch } = require('gulp');
+const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const terser = require('gulp-terser');
 const rename = require('gulp-rename');
+
+sass.compiler = require('sass');
 
 const configs = {
     autoprefixer: {
@@ -26,23 +29,24 @@ const configs = {
 };
 
 function minifycss(cb) {
-    src('src/**/*.css')
+    src('src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer(configs.autoprefixer))
         .pipe(cleanCSS(configs.cleanCSS))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(dest('source'));
+        .pipe(dest('source/css'));
     cb();
 }
 
 function minifyjs(cb) {
-    src('src/**/*.js')
+    src('src/js/*.js')
         .pipe(terser(configs.terser))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(dest('source'));
+        .pipe(dest('source/js'));
     cb();
 }
 
-watch('src/**', parallel(minifycss, minifyjs));
+// watch('src/**', parallel(minifycss, minifyjs));
 
 exports.build = parallel(minifycss, minifyjs);
 exports.default = parallel(minifycss, minifyjs);
