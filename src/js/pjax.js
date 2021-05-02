8c9978f49c9cb65370eadbafb580190a766ad4cb
Candinya <dev@candinya.com>
2021-05-02 12:47:59 +0800

    fix: popstate blank after pjax fail #58

--- src/js/pjax.js
+++ src/js/pjax.js
@@ -1,19 +1,23 @@
 $(function() {
     const mainElement = '#kratos-blog-post .row';
     theTop = notMobile ? $("#kratos-blog-post").offset().top-40 : 0;
-    window.addEventListener('popstate', (e) => {
+
+    const popStateHandler = (e) => {
         if ( e.state ) {
+            console.log('popstate - triggered');
             document.title = e.state.title || document.title;
             $("body,html").animate({scrollTop:theTop}, 600);
-            if (typeof e.state.html === 'undefined') {
+            if (typeof e.state.content === 'undefined') {
                 ajax(e.state.url, false);
             } else {
-                $(mainElement).html( e.state.html );
+                $(mainElement).html( e.state.content );
             }
+            // 更新外域变量
             OriginTitile = document.title;
             window.dispatchEvent(pjaxEvents.complete);
         }
-    });
+    };
+    window.addEventListener('popstate', popStateHandler);
 
     const pjaxEvents = {
         before:   new Event('pjax:before')  ,
@@ -23,6 +27,7 @@ $(function() {
     };
 
     function ajax(reqUrl, needPushState) {
+
         $.ajax({
             url: reqUrl,
             beforeSend: function () {
@@ -32,35 +37,64 @@ $(function() {
                 if (typeof load_comm !== 'undefined' && load_comm !== null) {
                     load_comm = null;
                 }
+
+                // 运动到顶部
+                $("body,html").animate({scrollTop:theTop}, 600);
+
+                // 记录当前状态
                 history.replaceState({
                     url: location.href,
                     title: document.title,
-                    html: $(document).find(mainElement).html()
-                    }, document.title, location.href);
-                $("body,html").animate({scrollTop:theTop}, 600);
+                    content: document,
+                }, document.title, location.href);
+
+                // 进度条开始
                 NProgress.start();
             },
-            success: function(data) {
+            success: function(data, textStatus, jqXHR) {
                 window.dispatchEvent(pjaxEvents.success);
                 
                 if (typeof $(data).find(mainElement).html() === 'undefined') {
+                    // 停止当前 pjax 进程
+                    jqXHR.abort(404);
+                    // 直接跳转
                     location.href = reqUrl;
-                } else {
-                    $(mainElement).html($(data).find(mainElement).html());
-                }
-                document.title = $(data).filter("title").text();
-                if (needPushState) {
-                    window.history.pushState({
-                        url: reqUrl,
-                        title: $(data).filter("title").text(),
-                        html: $(data).find(mainElement).html()
-                    }, $(data).filter("title").text(), reqUrl);
                 }
             },
-            complete: function() {
+            complete: function(jqXHR, textStatus) {
                 window.dispatchEvent(pjaxEvents.complete);
 
+                const data = jqXHR.responseText;
+
+                // 替换记录的当前状态
+                history.replaceState({
+                    url: location.href,
+                    title: document.title,
+                    content: $(document).find(mainElement).html(),
+                }, document.title, location.href);
+
+                // 记录新状态的一些信息
+                const newTitle = $(data).filter("title").text();
+                const newContent = $(data).find(mainElement).html();
+
+                // 如果需要压入新的状态
+                if (needPushState) {
+                    // 压入新的状态
+                    window.history.pushState({
+                        url: reqUrl,
+                        title: newTitle,
+                        content: newContent,
+                    }, newTitle, reqUrl);
+                }
+
+                // 更新当前页面需要更新的内容
+                document.title = newTitle;
+                $(mainElement).html(newContent);
+
+                // 更新外域变量
                 OriginTitile = document.title;
+
+                // 进度条结束
                 NProgress.done();
                 
                 // 如果URL里有指定节点ID，则滚动到相应的节点位置
@@ -70,8 +104,10 @@ $(function() {
                 }
             },
             timeout: 6000,
-            error: function() {
+            error: function(jqXHR, textStatus, errorThrown) {
                 window.dispatchEvent(pjaxEvents.error);
+
+                console.error(errorThrown);
 
                 location.href = reqUrl;
             }
