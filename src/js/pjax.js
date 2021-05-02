ff6e158fcd9157043ab3b213c49fd09599679d0f
Candinya <dev@candinya.com>
2021-05-02 13:28:28 +0800

    fix: pjax fail

--- src/js/pjax.js
+++ src/js/pjax.js
@@ -28,10 +28,11 @@ $(function() {
 
     function ajax(reqUrl, needPushState) {
 
+        let isSuccessRequest = false;
+
         $.ajax({
             url: reqUrl,
             beforeSend: function () {
-                window.dispatchEvent(pjaxEvents.before);
 
                 // 防止评论区再被加载，重置加载函数
                 if (typeof load_comm !== 'undefined' && load_comm !== null) {
@@ -41,75 +42,77 @@ $(function() {
                 // 运动到顶部
                 $("body,html").animate({scrollTop:theTop}, 600);
 
-                // 记录当前状态
-                history.replaceState({
-                    url: location.href,
-                    title: document.title,
-                    content: document,
-                }, document.title, location.href);
-
                 // 进度条开始
                 NProgress.start();
+                
+                window.dispatchEvent(pjaxEvents.before);
             },
             success: function(data, textStatus, jqXHR) {
-                window.dispatchEvent(pjaxEvents.success);
                 
-                if (typeof $(data).find(mainElement).html() === 'undefined') {
-                    // 停止当前 pjax 进程
-                    jqXHR.abort(404);
-                    // 直接跳转
-                    location.href = reqUrl;
+                if (typeof $(data).find(mainElement).html() !== 'undefined') {
+                    // 这样才成功嘛
+                    isSuccessRequest = true;
                 }
+
+                window.dispatchEvent(pjaxEvents.success);
             },
             complete: function(jqXHR, textStatus) {
-                window.dispatchEvent(pjaxEvents.complete);
 
-                const data = jqXHR.responseText;
+                if (isSuccessRequest) {
+                    const data = jqXHR.responseText;
 
-                // 替换记录的当前状态
-                history.replaceState({
-                    url: location.href,
-                    title: document.title,
-                    content: $(document).find(mainElement).html(),
-                }, document.title, location.href);
+                    // 替换记录的当前状态
+                    history.replaceState({
+                        url: location.href,
+                        title: document.title,
+                        content: $(document).find(mainElement).html(),
+                    }, document.title, location.href);
 
-                // 记录新状态的一些信息
-                const newTitle = $(data).filter("title").text();
-                const newContent = $(data).find(mainElement).html();
+                    // 记录新状态的一些信息
+                    const newTitle = $(data).filter("title").text();
+                    const newContent = $(data).find(mainElement).html();
 
-                // 如果需要压入新的状态
-                if (needPushState) {
-                    // 压入新的状态
-                    window.history.pushState({
-                        url: reqUrl,
-                        title: newTitle,
-                        content: newContent,
-                    }, newTitle, reqUrl);
+                    // 如果需要压入新的状态
+                    if (needPushState) {
+                        // 压入新的状态
+                        window.history.pushState({
+                            url: reqUrl,
+                            title: newTitle,
+                            content: newContent,
+                        }, newTitle, reqUrl);
+                    }
+
+                    // 更新当前页面需要更新的内容
+                    document.title = newTitle;
+                    $(mainElement).html(newContent);
+
+                    // 更新外域变量
+                    OriginTitile = document.title;
+                    
+                    // 如果URL里有指定节点ID，则滚动到相应的节点位置
+                    const reqId = reqUrl.match(/\#.+$/);
+                    if (reqId) {
+                        $("body,html").animate({scrollTop:$(reqId[0]).offset().top - 40}, 600);
+                    }
+                } else {
+                    // 其实是失败的
+                    // 不如打开一个新页面？
+                    window.open(reqUrl);
                 }
-
-                // 更新当前页面需要更新的内容
-                document.title = newTitle;
-                $(mainElement).html(newContent);
-
-                // 更新外域变量
-                OriginTitile = document.title;
 
                 // 进度条结束
                 NProgress.done();
-                
-                // 如果URL里有指定节点ID，则滚动到相应的节点位置
-                const reqId = reqUrl.match(/\#.+$/);
-                if (reqId) {
-                    $("body,html").animate({scrollTop:$(reqId[0]).offset().top - 40}, 600);
-                }
+
+                window.dispatchEvent(pjaxEvents.complete);
             },
             timeout: 6000,
             error: function(jqXHR, textStatus, errorThrown) {
-                window.dispatchEvent(pjaxEvents.error);
 
                 console.error(errorThrown);
 
                 location.href = reqUrl;
+                
+                window.dispatchEvent(pjaxEvents.error);
             }
         });
     }
