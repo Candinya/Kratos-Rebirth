949f78035bced1bc6ef0bc382092e10e1878e9f3
Nya Candy <dev@candinya.com>
2021-12-24 20:26:27 +0800

    fix: wrong local path

--- scripts/cdn-optimize-helpers.js
+++ scripts/cdn-optimize-helpers.js
@@ -2,28 +2,28 @@ const path = require('path');
 
 const { version } = require(path.normalize('../package.json'));
 
-const npmCDNHandler = (file, root, cdnConfig) => {
-    let cdnBase = root;
+const npmCDNHandler = (defaultRoot, cdnConfig, cdnSuffix = '') => {
+    let cdnBase = defaultRoot;
     if (cdnConfig) {
         switch (cdnConfig.toLowerCase()) {
             case 'unpkg':
-                cdnBase = 'https://unpkg.com/';
+                cdnBase = 'https://unpkg.com/' + cdnSuffix;
                 break;
             case 'jsdelivr':
             default:
-                cdnBase = 'https://cdn.jsdelivr.net/npm/';
+                cdnBase = 'https://cdn.jsdelivr.net/npm/' + cdnSuffix;
                 break;
         }
     }
-    return cdnBase + file;
+    return cdnBase;
 }
 
 // 使用CDN提供静态资源，改写URL
 hexo.theme.on('processAfter', () => {
     hexo.extend.helper.register('url_cdn', (file) => {
-        return npmCDNHandler(`kratos-rebirth@${version}/source/` + file, hexo.config.root, hexo.theme.config.cdn);
+        return npmCDNHandler(hexo.config.root, hexo.theme.config.cdn, `kratos-rebirth@${version}/source/`) + file;
     });
     hexo.extend.helper.register('url_npm_cdn', (file) => {
-        return npmCDNHandler(file, 'https://cdn.jsdelivr.net/npm/', hexo.theme.config.cdn);
+        return npmCDNHandler('https://cdn.jsdelivr.net/npm/', hexo.theme.config.cdn) + file;
     });
 });
