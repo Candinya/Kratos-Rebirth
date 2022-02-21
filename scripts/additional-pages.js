e0b2e2ceb65fc11ff766a04582167323325fdb6e
Nya Candy <dev@candinya.com>
2022-02-21 14:54:58 +0800

    fix: tags & categories page route

--- scripts/additional-pages.js
+++ scripts/additional-pages.js
@@ -13,7 +13,7 @@ hexo.extend.generator.register('page-notfound', function(locals){
 // 生成 tags 页面
 hexo.extend.generator.register('page-tags', function(locals){
     return {
-        path: 'tags/index.html',
+        path: hexo.route.format(hexo.config.tag_dir + '/'),
         data: locals.posts,
         layout: '_pages/tags',
     };
@@ -22,7 +22,7 @@ hexo.extend.generator.register('page-tags', function(locals){
 // 生成 categories 页面
 hexo.extend.generator.register('page-categories', function(locals){
     return {
-        path: 'categories/index.html',
+        path: hexo.route.format(hexo.config.category_dir + '/'),
         data: locals.posts,
         layout: '_pages/categories',
     };
