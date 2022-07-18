53b337c64ab91e4557fde14e211a7e0a48402b8a
ArcticLampyrid <ArcticLampyrid@outlook.com>
2022-07-19 01:48:58 +0800

    enhance: improve browser compatibility

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,25 @@
 const { src, dest, parallel, watch } = require('gulp');
 const sass = require('gulp-dart-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
 const terser = require('gulp-terser');
 const rename = require('gulp-rename');
+const babel = require("gulp-babel");
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
 sass.compiler = require('sass');
 
 const configs = {
-    autoprefixer: {
-        overrideBrowserslist: [
-            'last 2 versions',
-            '> 1%',
-            'Chrome >= 30',
-            'Firefox >= 30',
-            'ie >= 9',
-            'Safari >= 8',
-        ],
-    },
-    cleanCSS: {
-        level: 2
-    },
+    autoprefixer: {},
+    cssnano: {},
     terser: {
         output: {
             comments: /^!/
-        }
+        },
+        ie8: true,
+        safari10: true
     }
 };
 
@@ -34,8 +27,10 @@ function buildSass(cb) {
     src('src/scss/*.scss')
         .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
-            .pipe(autoprefixer(configs.autoprefixer))
-            .pipe(cleanCSS(configs.cleanCSS))
+            .pipe(postcss([
+                autoprefixer(configs.autoprefixer),
+                cssnano(configs.cssnano)
+            ]))
             .pipe(rename({ suffix: '.min' }))
         .pipe(sourcemaps.write('../maps'))
         .pipe(dest('source/css'))
@@ -47,8 +42,10 @@ function buildHighlight(cb) {
     src('src/scss/highlight/theme/*.scss')
         .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
-            .pipe(autoprefixer(configs.autoprefixer))
-            .pipe(cleanCSS(configs.cleanCSS))
+            .pipe(postcss([
+                autoprefixer(configs.autoprefixer),
+                cssnano(configs.cssnano)
+            ]))
             .pipe(rename({ suffix: '.min' }))
         .pipe(sourcemaps.write('../../maps'))
         .pipe(dest('source/css/highlight'))
@@ -59,6 +56,9 @@ function buildHighlight(cb) {
 function minifyJs(cb) {
     src('src/js/*.js')
         .pipe(sourcemaps.init())
+            .pipe(babel({
+                presets: ["@babel/preset-env"]
+            }))
             .pipe(terser(configs.terser))
             .pipe(rename({ suffix: '.min' }))
         .pipe(sourcemaps.write('../maps'))
