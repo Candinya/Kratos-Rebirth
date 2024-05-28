a2bd90b67904c5ba99d3a5f10edcf63d05555a74
Nya Candy <dev@candinya.com>
2024-05-29 00:04:01 +0800

    chore: optimize search

--- scripts/search.js
+++ scripts/search.js
@@ -1,105 +1,95 @@
+const { stripHTML } = require("hexo-util");
+
+const parseSearchItem = (item, includeContent, url_for) => {
+  let _item = {};
+  if (item.title) {
+    _item.title = item.title;
+  }
+  if (item.date) {
+    _item.date = item.date;
+  }
+  if (item.path) {
+    _item.url = url_for(item.path);
+  }
+  if (item.tags && item.tags.length > 0) {
+    _item.tags = [];
+    item.tags.forEach((tag) => {
+      _item.tags.push([tag.name, url_for(tag.path)]);
+    });
+  }
+  _item.categories = [];
+  if (item.categories && item.categories.length > 0) {
+    item.categories.forEach((cate) => {
+      _item.categories.push([cate.name, url_for(cate.path)]);
+    });
+  } else {
+    _item.categories.push(["undefined", ""]);
+  }
+  if (includeContent && item.content) {
+    _item.content = stripHTML(
+      item.content.trim().replace(/<pre(.*?)\<\/pre\>/gs, ""),
+    )
+      .replace(/\n/g, " ")
+      .replace(/\s+/g, " ")
+      .replace(
+        new RegExp(
+          "(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]",
+          "g",
+        ),
+        "",
+      );
+  }
+  return _item;
+};
+
 hexo.once("generateBefore", () => {
-  if (!hexo.theme.config.search || !hexo.theme.config.search.enable) {
-    return;
-  }
+  if (hexo.theme.config.search.provider === "local") {
+    // 生成搜索数据库
 
-  const pathFn = require("path");
-  const { stripHTML } = require("hexo-util");
+    let config = hexo.theme.config.search;
 
-  let config = hexo.theme.config.search;
+    // 设置默认搜索路径
+    if (!config.path.index_file) {
+      config.path.index_file = "search-index.json";
+    }
 
-  // 生成搜索数据库
-
-  // 设置默认搜索路径
-  if (!config.index_path) {
-    config.index_path = "search-index.json";
-  }
-
-  if (pathFn.extname(config.index_path) === ".json") {
     hexo.extend.generator.register("searchIndex", function (locals) {
       const url_for = hexo.extend.helper.get("url_for").bind(this);
 
-      const parse = (item) => {
-        let _item = {};
-        if (item.title) _item.title = item.title;
-        if (item.date) _item.date = item.date;
-        if (item.path) _item.url = url_for(item.path);
-        if (item.tags && item.tags.length > 0) {
-          _item.tags = [];
-          item.tags.forEach((tag) => {
-            _item.tags.push([tag.name, url_for(tag.path)]);
-          });
-        }
-        _item.categories = [];
-        if (item.categories && item.categories.length > 0) {
-          item.categories.forEach((cate) => {
-            _item.categories.push([cate.name, url_for(cate.path)]);
-          });
-        } else {
-          _item.categories.push(["undefined", ""]);
-        }
-        if (hexo.theme.config.search.content && item.content) {
-          _item.content = stripHTML(
-            item.content.trim().replace(/<pre(.*?)\<\/pre\>/gs, ""),
-          )
-            .replace(/\n/g, " ")
-            .replace(/\s+/g, " ")
-            .replace(
-              new RegExp(
-                "(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]",
-                "g",
-              ),
-              "",
-            );
-        }
-        return _item;
-      };
-
-      const searchfield = config.field;
-
-      let posts, pages;
-
-      if (searchfield) {
-        if (searchfield === "post") {
-          posts = locals.posts.sort("-date");
-        } else if (searchfield === "page") {
-          pages = locals.pages;
-        } else {
-          posts = locals.posts.sort("-date");
-          pages = locals.pages;
-        }
-      } else {
-        posts = locals.posts.sort("-date");
-      }
+      const searchIncludes = config.includes;
 
       let res = [];
 
-      if (posts) {
-        posts.each((post) => {
-          res.push(parse(post));
+      if (searchIncludes.includes("post")) {
+        locals.posts.sort("-date").each((post) => {
+          res.push(
+            parseSearchItem(post, hexo.theme.config.search.content, url_for),
+          );
         });
       }
-      if (pages) {
-        pages.each((page) => {
-          res.push(parse(page));
+      if (searchIncludes.includes("page")) {
+        locals.pages.each((page) => {
+          res.push(
+            parseSearchItem(page, hexo.theme.config.search.content, url_for),
+          );
         });
       }
 
       return {
-        path: config.index_path,
+        path: config.path.index_file,
         data: JSON.stringify(res),
       };
     });
   }
 
-  // 生成搜索页面
-  hexo.extend.generator.register("searchPage", function (locals) {
-    return {
-      path: "search/index.html",
+  if (hexo.theme.config.search.provider !== "none") {
+    // 生成搜索页面
+    hexo.extend.generator.register("searchPage", (locals) => ({
+      path: `${hexo.theme.config.search.path.page}/index.html`,
       data: {
         title_i18n: "title.search",
       },
       layout: "_pages/search",
-    };
-  });
+    }));
+  }
 });
