fa06900274c194a5e3ff95a5717f62070d1f2e7e
Nya Candy <dev@candinya.com>
2024-09-20 21:26:16 +0800

    feat: inject permanent link to comment templates

--- scripts/process-template.js
+++ scripts/process-template.js
@@ -1,10 +1,22 @@
 const { createHash } = require("crypto");
 
-hexo.extend.helper.register("process_template_path", (template, path, type) => {
-  const hasher = hexo.theme.config[type].path_hasher;
-  const hash = createHash(hasher);
-  hash.update(path);
-  const digest = hash.digest("hex");
+hexo.extend.helper.register(
+  "process_template_path",
+  function (template, path, type) {
+    const url_for = hexo.extend.helper.get("url_for").bind(this);
+    const full_url_for = hexo.extend.helper.get("full_url_for").bind(this);
 
-  return template.replaceAll("$PATH_HASH", digest).replaceAll("$PATH", path);
-});
+    const pathWithRoot = url_for(path);
+    const pathFull = full_url_for(path);
+
+    const hasher = hexo.theme.config[type].path_hasher;
+    const hash = createHash(hasher);
+    hash.update(pathWithRoot);
+    const digest = hash.digest("hex");
+
+    return template
+      .replaceAll("$PATH_HASH", digest)
+      .replaceAll("$PATH_FULL", pathFull)
+      .replaceAll("$PATH", pathWithRoot);
+  },
+);
